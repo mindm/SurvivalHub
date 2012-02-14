aa30afeb108ad87237c287e3893a4013e62edf05
MindM <maindi66@gmail.com>
2012-02-14 12:44:53 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurvivalHub::Application.config.secret_token = '310bb95c6a797777b9e3e7c9d02d0d39d0873ed72fd5ed9f2cad7552c7d36987e1c347631c4dfb012a061b7ef87e665d306ac77eed53923caf95b595262a6517'
